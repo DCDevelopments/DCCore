31343466661b9a11c499233814ab48b732964fc2
cristian ramos <cristianramos83@gmail.com>
2017-07-10 20:44:38 -0300

    cambios para no tener problemas con sendgrid

--- DCCore.WebMvc/Utils/ExchangeEmailService.cs
+++ DCCore.WebMvc/Utils/ExchangeEmailService.cs
@@ -11,16 +11,10 @@ namespace DCCore.WebMvc.Utils
 {
     public class ExchangeEmailService : IIdentityMessageService
     {
-        //sendGrid
-        String APIKey = "apikey";
-        String SecretKey ="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";
-        //Host=smtp.sendgrid.net
+        
 
 
-        //MailJet
-        //String APIKey = "2272066c6f5b15339ca799e8037a5b6c";
-        //String SecretKey = "3583d8d4e0fbcb5770ca4734d5d0d9f8";
-        //Host=in.mailjet.com
+        
 
 
         public enum Mailboxes
@@ -34,12 +28,12 @@ namespace DCCore.WebMvc.Utils
         {
             using (var client = new SmtpClient())
             {
-                client.Host = "smtp.sendgrid.net";
+                client.Host = "";
                 client.Port = 25;
                 client.EnableSsl = true;
                 //client.DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory;
                 client.UseDefaultCredentials = false;
-                client.Credentials = new NetworkCredential(APIKey, SecretKey);
+                client.Credentials = new NetworkCredential("pepe", "pepesecret");
 
                 var from = new MailAddress("no-reply@example.com");
                 var to = new MailAddress(email);
